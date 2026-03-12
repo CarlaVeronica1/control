8e406d63d2005a5b41273043e0c8036c18fe64b4
CarlaVeronica1 <163350931+CarlaVeronica1@users.noreply.github.com>
2026-03-12 09:48:05 -0600

    a ver si si

--- index.js
+++ index.js
@@ -19,7 +19,9 @@ const authRoutes = require('./routes/auth');
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+//app.use(express.json());
+app.use(express.json({ limit: "1mb" }));
+app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
   res.send('¡Hola! El servidor está funcionando correctamente.');
 });
